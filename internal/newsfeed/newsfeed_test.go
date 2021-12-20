107501d7976bebac98ba89eadd39ffe48a747f5d
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-20 09:47:49 +0330

    newsfeed unit testing file added

---
+++ internal/newsfeed/newsfeed_test.go
@@ -0,0 +1,23 @@
+package newsfeed
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	feed := New()
+	feed.Add(Item{})
+
+	if len(feed.Items) != 1 {
+		t.Errorf("Item was not added in repository")
+	}
+}
+
+func TestGetAll(t *testing.T) {
+	feed := New()
+	feed.Add(Item{})
+
+	results := feed.GetAll()
+
+	if len(results) != 1 {
+		t.Errorf("Results wasn't return")
+	}
+}

c00795bb39f2775a72b09dcc132c8108878d8305
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-20 09:59:38 +0330

    routes added

--- cmd/main.go
+++ cmd/main.go
@@ -9,6 +9,8 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/ping", handler.PingGet())
+	r.GET("/newsfeed", handler.NewsFeedGet())
+	r.POST("/newsfeed", handler.NewsFeedPost())
 
 	_ = r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }

a75814d4845150a4df4c4592ad6aef9c024f28db
amirhnajafiz <najafizadeh21@gmail.com>
2023-08-05 12:20:47 +0330

    fix: db connection

--- service/main.py
+++ service/main.py
@@ -1,4 +1,5 @@
 import json
+import mysql.connector
 
 
 
@@ -11,3 +12,21 @@ f.close()
 
 # display configs
 print(json.dumps(config, indent=4))
+
+# open database connection
+connection = mysql.connector.connect(
+  host=config['DB_HOST'],
+  port=config['DB_PORT'],
+  user=config['DB_USER'],
+  password=config['DB_PASS'],
+  database=config['DB_NAME']
+)
+
+# open a cursor
+cursor = connection.cursor()
+
+# check tables
+cursor.execute("SHOW TABLES")
+
+for x in cursor:
+  print(x)

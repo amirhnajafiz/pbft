37af67b537cea6076de3b55cff9e25bbd50de8d4
amirhnajafiz <najafizadeh21@gmail.com>
2023-08-06 17:13:23 +0330

    add: insert query

---
+++ service/database/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO transactions (Reference, SourceAccount, DestinationAccount, Amount, Date, SourceBank, DestinationBank, Type) 
+    VALUES (?, ?, ?, ?, ?, ?, ? ,?);
--- service/provider.py
+++ service/provider.py
@@ -1,6 +1,7 @@
 class Provider(object):
     def __init__(self, batch):
         self.batch = batch
+        self.query = ""
     
-    def generate(self):
+    def generate(self, db):
         pass

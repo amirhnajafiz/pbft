18581e71fb6859b2252a6a5381350ec3becb25bd
amirhnajafiz <najafizadeh21@gmail.com>
2023-08-06 17:09:11 +0330

    add: transaction class

--- service/model.py
+++ service/model.py
@@ -1,6 +1,22 @@
 class Transatcion(object):
-    def __init__(self):
-        pass
+    def __init__(self, reference, source, dest, amount, date, sbank, dbank, type):
+        self.reference = reference
+        self.source = source
+        self.dest = dest
+        self.amount = amount
+        self.date = date
+        self.sbank = sbank
+        self.dbank = dbank
+        self.type = type
     
     def list(self):
-        pass
+        return [
+            self.reference,
+            self.source,
+            self.dest,
+            self.amount,
+            self.date,
+            self.sbank,
+            self.dbank,
+            self.type
+        ]

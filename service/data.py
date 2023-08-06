bedcd5c34b294de63310de98fec1cbe809474d35
amirhnajafiz <najafizadeh21@gmail.com>
2023-08-06 17:50:22 +0330

    add: data module

--- service/data.py
+++ service/data.py
@@ -5,14 +5,6 @@ import time
 
 
 def str_time_prop(start, end, time_format, prop):
-    """Get a time at a proportion of a range of two formatted times.
-
-    start and end should be strings specifying times formatted in the
-    given format (strftime-style), giving an interval [start, end].
-    prop specifies how a proportion of the interval to be taken after
-    start.  The returned time will be in the specified format.
-    """
-
     stime = time.mktime(time.strptime(start, time_format))
     etime = time.mktime(time.strptime(end, time_format))
 
@@ -21,10 +13,27 @@ def str_time_prop(start, end, time_format, prop):
     return time.strftime(time_format, time.localtime(ptime))
 
 
+def random_date(start, end, prop):
+    return str_time_prop(start, end, '%m/%d/%Y %I:%M %p', prop)
+
 
 class Data(object):
     def __init__(self):
         self.fake = Faker()
+        self.banks = [
+            'world bank',
+            'bank of america',
+            'blue bank',
+            'chase bank',
+            'visa card',
+            'paypal',
+            'american express',
+            'bancrop',
+            'saman',
+            'first boston',
+            'lehman brothers'
+        ]
+        self.types = ['atm', 'bank', 'app']
     
     def reference(self):
         return self.fake.bothify(text='#########')
@@ -36,4 +45,10 @@ class Data(object):
         return random.randint(20, 9999999)
 
     def date(self):
-        return 
+        return random_date("1/1/2021 00:00 AM", "1/1/2022 11:59 PM", random.random())
+    
+    def bank(self):
+        return random.choice(self.banks)
+    
+    def rtype(self):
+        return random.choice(self.type)

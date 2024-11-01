53a689fdb7b3747becac0ad1902bb45d62c869c6
amirhnajafiz <najafizadeh21@gmail.com>
2024-11-01 15:43:37 -0400

    add: node setup

--- cmd/node.go
+++ cmd/node.go
@@ -2,6 +2,9 @@ package cmd
 
 import (
 	"github.com/f24-cse535/pbft/internal/config"
+	"github.com/f24-cse535/pbft/internal/consensus"
+	"github.com/f24-cse535/pbft/internal/grpc"
+	"github.com/f24-cse535/pbft/internal/storage/local"
 
 	"go.uber.org/zap"
 )
@@ -13,5 +16,21 @@ type Node struct {
 }
 
 func (n Node) Main() error {
-	return nil
+	// create a local storage (aka memory)
+	mem := local.NewMemory()
+
+	// create a new consensus module
+	instance := consensus.Consensus{
+		Memory: mem,
+		Logger: n.Logger.Named("consensus"),
+	}
+
+	// create a new gRPC bootstrap instance and execute the server by running the boot commands
+	boot := grpc.Bootstrap{
+		Port:      n.Cfg.Node.Port,
+		Logger:    n.Logger.Named("grpc"),
+		Consensus: &instance,
+	}
+
+	return boot.ListenAnsServer()
 }

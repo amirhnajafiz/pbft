a29047991fc9b993964c2731f14fe3c2a583b75e
amirhnajafiz <najafizadeh21@gmail.com>
2024-11-03 12:13:55 -0500

    add: responses

--- internal/consensus/utils.go
+++ internal/consensus/utils.go
@@ -2,6 +2,7 @@ package consensus
 
 import (
 	"github.com/f24-cse535/pbft/internal/utils/hashing"
+	"github.com/f24-cse535/pbft/pkg/enum"
 	"github.com/f24-cse535/pbft/pkg/rpc/pbft"
 )
 
@@ -10,9 +11,9 @@ func (c *Consensus) getCurrentLeader() string {
 	return c.Memory.GetNodeByIndex(c.Memory.GetView())
 }
 
-// canExecute gets a sequence number and checks if all logs before that
-// are executed or not.
+// canExecute gets a sequence number and checks if all logs before that are executed or not.
 func (c *Consensus) canExecute(sequence int) bool {
+	// loop from the first sequence and check the execution status
 	for i := 0; i < sequence; i++ {
 		if tmp := c.Logs.GetLog(i); tmp != nil && tmp.Request.Status != pbft.RequestStatus_REQUEST_STATUS_E {
 			return false
@@ -24,16 +25,18 @@ func (c *Consensus) canExecute(sequence int) bool {
 
 // executeRequest takes a request message and executes its transaction.
 func (c *Consensus) executeRequest(msg *pbft.RequestMsg) {
+	// fetch all balance values
 	senderBalance := c.Memory.GetBalance(msg.GetTransaction().GetSender())
 	receiverBalance := c.Memory.GetBalance(msg.GetTransaction().GetReciever())
 	amount := msg.GetTransaction().GetAmount()
 
-	if amount > int64(senderBalance) {
-		msg.GetResponse().Text = "not enough balance"
-	} else {
+	msg.GetResponse().Text = enum.RespBadBalance
+
+	// check if the balance is sufficient
+	if amount <= int64(senderBalance) {
 		c.Memory.SetBalance(msg.GetTransaction().GetSender(), senderBalance-int(amount))
 		c.Memory.SetBalance(msg.GetTransaction().GetReciever(), receiverBalance+int(amount))
-		msg.GetResponse().Text = "submitted"
+		msg.GetResponse().Text = enum.RespSuccess
 	}
 }
 
---
+++ pkg/enum/responses.go
@@ -0,0 +1,7 @@
+package enum
+
+// responses are the text values that are returned to the clients.
+const (
+	RespBadBalance = "declined the client does not have enough balance"
+	RespSuccess    = "transaction submitted"
+)

8b92701f359902fbc719268061405e3e2c19ec71
amirhnajafiz <najafizadeh21@gmail.com>
2024-11-06 18:18:51 -0500

    fix: node configs load

--- cmd/node.go
+++ cmd/node.go
@@ -21,8 +21,8 @@ type Node struct {
 func (n Node) Main() error {
 	// create a local storage (aka memory)
 	mem := local.NewMemory(n.Cfg.Node.NodeId, n.Cfg.Node.BFT.Total)
-	mem.SetBalances(n.Cfg.IPTable.GetClientsMeta())
-	mem.SetNodes(n.Cfg.IPTable.GetNodesMeta())
+	mem.SetBalances(n.Cfg.GetClients())
+	mem.SetNodes(n.Cfg.GetNodesMeta())
 
 	// create a datalog instance
 	datalog := logs.NewLogs()
@@ -31,8 +31,7 @@ func (n Node) Main() error {
 	cli := client.NewClient(
 		n.Logger.Named("client.go"),
 		n.Cfg.Node.NodeId,
-		n.Cfg.IPTable.GetNodes(),
-		n.Cfg.IPTable.GetClients(),
+		n.Cfg.GetNodes(),
 	)
 	if err := cli.LoadTLS(n.Cfg.TLS.PrivateKey, n.Cfg.TLS.PublicKey, n.Cfg.TLS.CaKey); err != nil {
 		return err
@@ -47,8 +46,13 @@ func (n Node) Main() error {
 		Memory:     mem,
 		Logs:       datalog,
 		Logger:     n.Logger.Named("grpc"),
-		Consensus:  consensus.NewConsensus(datalog, mem, n.Logger.Named("consensus"), &n.Cfg.Node.BFT, cli),
-		Core:       core.NewCore(mem, cli, n.Logger.Named("core"), n.Cfg.Node.BFT.Responses),
+	}
+
+	// for core nodes we use consensus, for clients we use core
+	if n.Cfg.Node.CoreNode {
+		boot.Consensus = consensus.NewConsensus(datalog, mem, n.Logger.Named("consensus"), &n.Cfg.Node.BFT, cli)
+	} else {
+		boot.Core = core.NewCore(mem, cli, n.Logger.Named("core"), n.Cfg.Node.BFT.Responses)
 	}
 
 	return boot.ListenAnsServer()
